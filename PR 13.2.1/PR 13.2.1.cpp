bb7b42204d8d9488eab89a5908d81d8b0d3e5aa4
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-11-22 20:44:17 +0200

    first master commit

---
+++ PR 13.2.1/PR 13.2.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define abs_val(a) ((a) >= 0 ? (a) : -(a))
+
+#define POWER(x, y) pow(x, y)
+#define SQUARE(x) ((x) * (x))
+
+int main() {
+    float x, z;
+    cout << "Enter the value of x: ";
+    cin >> x;
+    cout << "Enter the value of z (should be between 5 and 10): ";
+    cin >> z;
+
+    if (z > 5 && z < 10) {
+        float w = min(x, z) * max(x + z, x * z);
+        cout << "w = min(x, z) * max(x + z, x * z) = " << w << std::endl;
+    }
+    else if (z <= 5 || z >= 10) {
+        float w = SQUARE(max(abs_val(x - z), z));
+        cout << "w = max^2(|x - z|, z) = " << w << std::endl;
+    }
+    else {
+        cout << "Invalid value of z entered." << std::endl;
+    }
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cmath>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+float CalculateExpression(float x, float z) {
+    float result = 0.0;
+    if (z > 5 && z < 10) {
+        result = std::min(x, z) * std::max(x + z, x * z);
+    }
+    else if (z <= 5 || z >= 10) {
+        result = std::pow(std::max(std::abs(x - z), z), 2);
+    }
+    return result;
+}
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestExpressionCalculation)
+		{
+			// Test when z is between 5 and 10
+			float x1 = 3.0f;
+			float z1 = 7.0f;
+			float expectedResult1 = std::min(x1, z1) * std::max(x1 + z1, x1 * z1);
+			Assert::AreEqual(expectedResult1, CalculateExpression(x1, z1));
+
+			// Test when z is less than or equal to 5
+			float x2 = 6.0f;
+			float z2 = 3.0f;
+			float expectedResult2 = std::pow(std::max(std::abs(x2 - z2), z2), 2);
+			Assert::AreEqual(expectedResult2, CalculateExpression(x2, z2));
+
+			// Test when z is greater than or equal to 10
+			float x3 = 5.0f;
+			float z3 = 12.0f;
+			float expectedResult3 = std::pow(std::max(std::abs(x3 - z3), z3), 2);
+			Assert::AreEqual(expectedResult3, CalculateExpression(x3, z3));
+		}
+	};
+}
